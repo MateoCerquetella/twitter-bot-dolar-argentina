f585e07e8b8026f1dbe5ef467f921d9d64367537
Mateo Cerquetella <mateocerquetella@hotmail.com>
2020-11-03 14:50:10 -0300

    REFACTOR, function names & labels

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const Twitter = new Twit({
 (function main(): void {
   process.title = 'node-twitter-bot';
   console.log(`Started ${process.title} with PID ${process.pid}`);
-  postTweets();
+  schedulerJob();
 }());
 
 async function api<T>(url: string): Promise<T> {
@@ -27,7 +27,7 @@ async function api<T>(url: string): Promise<T> {
   return response.json() as Promise<T>;
 }
 
-async function getDolar(): Promise<Array<DolarTwitI> | void> {
+async function getApiDolar(): Promise<Array<DolarTwitI> | void> {
   try {
     const res = await api<DolarResponseI>('https://api.bluelytics.com.ar/v2/latest');
     return [{
@@ -43,70 +43,68 @@ async function getDolar(): Promise<Array<DolarTwitI> | void> {
   }
 }
 
-function postTweets() {
+async function schedulerJob(): Promise<void> {
   const DOLAR_RULE = getScheduleRule(
     {
       days: [new schedule.Range(1, 5)],
-      hours: [10, 15, 22],
-      minute: 30
+      hours: [10, 14, 19],
+      minute: 49
     });
 
-  schedule.scheduleJob(DOLAR_RULE!, async function () {
-    const DOLAR_ARRAY = await getDolar();
+  schedule.scheduleJob(DOLAR_RULE!, async function getDolarAndPost(): Promise<void> {
+    const DOLAR_ARRAY = await getApiDolar();
     if (!DOLAR_ARRAY) return;
 
     DOLAR_ARRAY.forEach(dolar => {
       const DOLAR_LABEL = getRandomText(dolar.dolarType, dolar.dolarValue);
-      postToTwitter(DOLAR_LABEL, dolar.dolarValue);
+      postToTwitter(DOLAR_LABEL, dolar.dolarType, dolar.dolarValue);
     });
   });
 }
 
-function postToTwitter(dolarLabel: string, dolarPrice: number) {
+function postToTwitter(dolarLabel: string, dolarType: string, dolarPrice: number): void {
   Twitter.post('statuses/update', { status: dolarLabel }, function () {
-    console.log(`Twitted dolar blue on $${dolarPrice} ${getNow()}`);
+    console.log(`Twitted dolar ${dolarType}
+                price: $${dolarPrice}
+                date: ${getNow()}`);
   });
 }
 
 function getScheduleRule(
-  { day,
-    days,
-    hour,
+  { days,
     hours,
     minute
   }: {
-    day?: number;
     days?: [schedule.Range];
-    hour?: number;
     hours?: Array<number>;
     minute: number;
   }): schedule.RecurrenceRule | null {
   const rule = new schedule.RecurrenceRule();
-  if (day && days === undefined) return null;
-  if (hour && hours === undefined) return null;
-  rule.dayOfWeek = day! || days!;
-  rule.hour = hour! || hours!;
+  if (days === undefined) return null;
+  if (hours === undefined) return null;
+  rule.dayOfWeek = days;
+  rule.hour = hours;
   rule.minute = minute;
   return rule;
 }
 
 function getRandomText(dolarType: string, dolarPrice: number): string {
   const DOLAR_LABEL = [
-    'Un dolar ' + dolarType + ' en este momento son $' + dolarPrice + ' ARS',
-    'Hoy, un dolar ' + dolarType + ' está $' + dolarPrice + ' ARS',
-    'En este momento, el dolar ' + dolarType + ' está a $' + dolarPrice + ' ARS',
-    'El dolar ' + dolarType + ' se encuentra a $' + dolarPrice + ' ARS',
-    'Un dolar ' + dolarType + ' cuesta $' + dolarPrice + ' ARS',
-    'Ahora, un dolar ' + dolarType + ' vale $' + dolarPrice + ' ARS',
-    'Al dia de hoy un dolar ' + dolarType + ' está a $' + dolarPrice + ' ARS',
-    'Un dolar ' + dolarType + ' al día de hoy equivale a $' + dolarPrice + ' ARS',
-    'Un dolar ' + dolarType + ' está a $' + dolarPrice + ' ARS',
-    'Hoy el dolar ' + dolarType + ' está a $' + dolarPrice + ' ARS',
-    'Un dolar ' + dolarType + ', $' + dolarPrice + ' ARS',
-    'Actualmente un dolar ' + dolarType + ' está a $' + dolarPrice + ' ARS',
-    'Actualmente, un dolar ' + dolarType + ' equivale a $' + dolarPrice + ' ARS',
-    'En el dia de hoy, un dolar ' + dolarType + ' vale $' + dolarPrice + ' ARS',
-    'El dolar ' + dolarType + ' equivale $' + dolarPrice + ' ARS',
+    `Un dolar ${dolarType} en este momento son $${dolarPrice} pesos argentinos`,
+    `Hoy, un dolar ${dolarType} está $${dolarPrice} pesos argentinos`,
+    `En este momento, el dolar ${dolarType} está a $${dolarPrice} pesos argentinos`,
+    `El dolar ${dolarType} se encuentra a $${dolarPrice} pesos argentinos`,
+    `Un dolar ${dolarType} cuesta $${dolarPrice} pesos argentinos`,
+    `Ahora, un dolar ${dolarType} vale $${dolarPrice} pesos argentinos`,
+    `Al dia de hoy un dolar ${dolarType} está a $${dolarPrice} pesos argentinos`,
+    `Un dolar ${dolarType} al día de hoy equivale a $${dolarPrice} pesos argentinos`,
+    `Un dolar ${dolarType} está a $${dolarPrice} pesos argentinos`,
+    `Hoy el dolar ${dolarType} está a $${dolarPrice} pesos argentinos`,
+    `Un dolar ${dolarType}, $${dolarPrice} pesos argentinos`,
+    `Actualmente un dolar ${dolarType} está a $${dolarPrice} pesos argentinos`,
+    `Actualmente, un dolar ${dolarType} equivale a $${dolarPrice} pesos argentinos`,
+    `En el dia de hoy, un dolar ${dolarType} vale $${dolarPrice} pesos argentinos`,
+    `El dolar ${dolarType} equivale $${dolarPrice} pesos argentinos`,
   ];
   return DOLAR_LABEL[Math.floor(Math.random() * DOLAR_LABEL.length)];
 };
